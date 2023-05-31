7e00e375e8b924f63a1557d7739bd49624fb7553
Micah Njeru <micahnjeru1@gmail.com>
2023-05-31 11:06:21 +0300

    Added new views, added navigation links from home view to child views and passed data (images+text) after decoding to child views. Also added dark mode.

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Micah Jesse Njeru on 31/05/2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.background)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -22,26 +22,40 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text("Detail View")
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundColor(.white)
+                                    
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
