2f27d41dd2392270aeec765f5831060e05f2b6d6
Micah Njeru <micahnjeru1@gmail.com>
2023-05-31 09:30:46 +0300

    Added JSON data files and asset images

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Micah Jesse Njeru on 31/05/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable {
+    let id: String
+    let name: String
+    let description: String
+}
