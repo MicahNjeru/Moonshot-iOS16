8d99187403518494311eaad0ae73cf70498b32c7
Micah Njeru <micahnjeru1@gmail.com>
2023-05-31 08:13:53 +0300

    Initial commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Micah Jesse Njeru on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
